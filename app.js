9e6e6e49010a18496f69c2cf62ca1d8f5b9d5307
Yash Sharma <yash4688@gmail.com>
2017-08-06 17:12:27 +0530

    using forecast.io api

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode.js');
+// const geocode = require('./geocode/geocode.js');
 
-const argv = yargs
-.options({
-    a: {
-        demand: true,
-        alias: 'address',
-        describe:'Address to fetch weather for',
-        string: true
-    }
-})
-.help()
-.alias('help','h')
-.argv;
+// const argv = yargs
+// .options({
+//     a: {
+//         demand: true,
+//         alias: 'address',
+//         describe:'Address to fetch weather for',
+//         string: true
+//     }
+// })
+// .help()
+// .alias('help','h')
+// .argv;
 
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-    if(errorMessage){
-        console.log(errorMessage);
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//     if(errorMessage){
+//         console.log(errorMessage);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+//3212884ca0206f388b72241335987fdd
+
+const request = require('request');
+
+request({
+    url: 'https://api.darksky.net/forecast/3212884ca0206f388b72241335987fdd/28.6930443,77.1350949',
+    json: true
+}, (error, response, body) => {
+    if(!error && reponse.statusCode === 200){
+        console.log(body.currently.temperature);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
-    }
+        console.log('Unable to fetch weather');
+    } 
 });
